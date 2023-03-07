89218b9d22a669f747814c4bf460cf9de2580166
LeandraVoges <127184530+LeandraVoges@users.noreply.github.com>
2023-03-07 10:08:23 +0100

    Add files via upload

---
+++ Übung4.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct  6 12:28:47 2022
+
+@author: VOL2SZ
+"""
+"""
+x = 4
+y = 80
+"""
+x = input("Zahl1 zum Tauschen eingeben")
+y = input("Zahl2 zum Tauschen eingeben")
+def tauschen(a, b):
+    chng = a
+    a = b
+    b = chng
+    print (a)
+    print (b)
+    
+tauschen(x , y)
